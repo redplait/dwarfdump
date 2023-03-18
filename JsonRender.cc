04b3a1d883619b871396b5384104919360eaaf5d
redp <redplait@gmail.com>
2023-03-18 10:01:55 +0300

    remade json renderer

--- JsonRender.cc
+++ JsonRender.cc
@@ -47,6 +47,35 @@ std::string JsonRender::GenerateJson() {
   return result;
 }
 
+template <class T>
+std::string &JsonRender::put(std::string &res, const char *name, T v)
+{
+  res += "\"";
+  res += name;
+  res += "\":\""+std::to_string(v);
+  res += "\",";
+  return res;
+}
+
+template <>
+std::string &JsonRender::put(std::string &res, const char *name, const char *v)
+{
+  res += "\"";
+  res += name;
+  res += "\":\""+EscapeJsonString(v);
+  res += "\",";
+  return res;
+}
+
+template <>
+std::string &JsonRender::put(std::string &res, const char *name, bool v)
+{
+  res += "\"";
+  res += name;
+  res += "\":\"1\",";
+  return res;
+}
+
 std::string JsonRender::GenerateJson(Element &e) {
   std::string result;
 
@@ -57,84 +86,75 @@ std::string JsonRender::GenerateJson(Element &e) {
   if (e.type_ == ElementType::member) {
     result = "{";
 
-    if (e.type_id_) {
-      result += "\"type_id\":\""+std::to_string(get_replaced_type(e.type_id_))+"\",";
-    }
-    if (e.name_) {
-      result += "\"name\":\""+EscapeJsonString(e.name_)+"\",";
-    }
-    if ( e.level_ && g_opt_l ) {
-      result += "\"level\":\""+std::to_string(e.level_)+"\",";
-    }
-    if ( e.access_ ) {
-      result += "\"access\":"+std::to_string(e.access_)+",";
-    }
+    if (e.type_id_)
+      put(result, "type_id", get_replaced_type(e.type_id_));
+    if (e.name_)
+      put(result, "name", e.name_);
+    if ( e.level_ && g_opt_l )
+      put(result, "level", e.level_);
+    if ( e.access_ )
+      put(result, "access", e.access_);
     if ( e.bit_size_ )
     {
-      result += "\"bit_offset\":"+std::to_string(e.bit_offset_)+",";
-      result += "\"bit_size\":"+std::to_string(e.bit_size_)+",";
+      put(result, "bit_offset", e.bit_offset_);
+      put(result, "bit_size", e.bit_size_);
     }
     result += "\"offset\":"+std::to_string(e.offset_);
-
     result += "}";
     return result;
   }
 
-  // The others are generic
-  result += "\""+std::to_string(e.id_)+"\":";
-  result += "{\"type\":\""+std::string(e.TypeName())+"\",";
+  // The others are generic  
+  result += "\""+std::to_string(e.id_)+"\":{";
+  put(result, "type", e.TypeName());
   if ( e.type_ == ElementType::ptr2member && e.cont_type_ )
-    result += "\"cont_type\":\""+std::to_string(e.cont_type_)+"\",";
+    put(result, "cont_type", e.cont_type_);
   if ( e.owner_ != nullptr )
-    result += "\"owner\":\""+std::to_string(e.owner_->id_)+"\",";
+    put(result, "owner", e.owner_->id_);
   if ( e.noret_ )
-    result += "\"noreturn\",";
+    put(result, "noreturn", e.noret_);
   if ( e.align_ )
-    result += "\"alignment\":\""+std::to_string(e.align_)+"\",";
-  if (e.type_id_) {
-    result += "\"type_id\":\""+std::to_string(e.type_id_)+"\",";
-  }
+    put(result, "alignment", e.align_);
+  if (e.type_id_)
+    put(result, "type_id", e.type_id_);
   if (e.name_) {
       if (e.type_ == ElementType::ns_start) {
         result += "\"name\":\""+EscapeJsonString(e.name_)+"\"}";
         return result;
       }
-    result += "\"name\":\""+EscapeJsonString(e.name_)+"\",";
+    put(result, "name", e.name_);
   }
-  if ( e.link_name_ && e.link_name_ != e.name_ ) {
-    result += "\"link_name\":\""+EscapeJsonString(e.link_name_)+"\",";
-  }
+  if ( e.link_name_ && e.link_name_ != e.name_ )
+    put(result, "link_name", e.link_name_);
   if (e.spec_)
-    result += "\"spec\":"+std::to_string(e.spec_)+",";
+    put(result, "spec", e.spec_);
   if ( e.abs_ )
-    result += "\"abs\":"+std::to_string(e.abs_)+",";
-  if (e.size_) {
-    result += "\"size\":"+std::to_string(e.size_)+",";
-  }
-  if ( e.addr_ ) {
-    result += "\"addr\":"+std::to_string(e.addr_)+",";
-  }
+    put(result, "abs", e.abs_);
+  if (e.size_)
+    put(result, "size", e.size_);
+  if ( e.addr_ )
+    put(result, "addr", e.addr_);
   if ( e.type_ == ElementType::method )
   {
     if ( g_opt_l )
-      result += "\"level\":\""+std::to_string(e.level_)+"\",";
+      put(result, "level", e.level_);
     Method &m = static_cast<Method &>(e);
     if ( m.vtbl_index_ )
-      result += "\"vtbl_index\":"+std::to_string(m.vtbl_index_)+",";
+      put(result, "vtbl_index", m.vtbl_index_);
     if ( m.virt_ )
-      result += "\"virt\":"+std::to_string(m.virt_)+",";
+      put(result, "virt", m.virt_);
     if ( m.this_arg_ )
-      result += "\"this_arg\":"+std::to_string(m.this_arg_)+",";
+      put(result, "this_arg", m.this_arg_);
   }
-  if (e.count_) {
-    result += "\"count\":"+std::to_string(e.count_)+",";
-  }
+  if (e.count_)
+    put(result, "count", e.count_);
+  // parents
   if ( e.m_comp && !e.m_comp->parents_.empty() ) {
     result += "\"parents\":[";
     for (size_t i = 0; i < e.m_comp->parents_.size(); i++) {
       result += "{\"id\":\""+std::to_string(get_replaced_type(e.m_comp->parents_[i].id))+"\",";
       if ( e.m_comp->parents_[i].access )
-        result += "\"access\":"+std::to_string(e.m_comp->parents_[i].access)+"\",";
+        put(result, "access", e.m_comp->parents_[i].access);
       result += "\"offset\":"+std::to_string(e.m_comp->parents_[i].offset)+"}";
       if (i+1 < e.m_comp->parents_.size()) {
         result += ",";
@@ -146,18 +166,17 @@ std::string JsonRender::GenerateJson(Element &e) {
   {
     result += "\"params\":[";
     for (size_t i = 0; i < e.m_comp->params_.size(); i++) {
+      result += "{";
       if ( e.m_comp->params_[i].name )
-        result += "{\"name\":\""+EscapeJsonString(e.m_comp->params_[i].name)+"\",";
-      else
-        result += "{";
+        put(result, "name", e.m_comp->params_[i].name);
       if ( e.m_comp->params_[i].param_id )
-        result += "\"id\":\""+std::to_string(e.m_comp->params_[i].param_id)+"\",";
+        put(result, "id", e.m_comp->params_[i].param_id);
       if (e.m_comp->params_[i].ellipsis)
       {
-        result += "\"ellipsis\"";
+        put(result, "ellipsis", e.m_comp->params_[i].ellipsis);
       } else {
         if ( e.m_comp->params_[i].id )
-          result += "\"type_id\":"+std::to_string(e.m_comp->params_[i].id);
+          put(result, "type_id", e.m_comp->params_[i].id);
       }
       if (result.back() == ',')
         result.pop_back();
@@ -172,7 +191,7 @@ std::string JsonRender::GenerateJson(Element &e) {
   {
     result += "\"enums\":[";
     for (size_t i = 0; i < e.m_comp->enums_.size(); i++) {
-      result += "{\"name\":\""+EscapeJsonString(e.m_comp->enums_[i].name)+"\",";
+      put(result, "name", e.m_comp->enums_[i].name);
       result += "\"value\":"+std::to_string(e.m_comp->enums_[i].value)+"}";
       if (i+1 < e.m_comp->enums_.size()) {
         result += ",";
--- JsonRender.h
+++ JsonRender.h
@@ -9,4 +9,6 @@ class JsonRender: public TreeBuilder
     virtual void RenderUnit(int last);
     static std::string EscapeJsonString(const char* str);
     std::string GenerateJson(Element &);
+    template <class T>
+    std::string &put(std::string &, const char *, T);
 };
