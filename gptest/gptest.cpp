467d8646f9196bb4609c21a6ac633067e07d59e3
redp <redplait@gmail.com>
2023-07-22 16:23:22 +0300

    add params dumping

--- gptest/gptest.cpp
+++ gptest/gptest.cpp
@@ -1,6 +1,8 @@
 // This is the first gcc header to be included
 #include "gcc-plugin.h"
 #include "plugin-version.h"
+#include "print-rtl.h"
+#include "langhooks.h"
 #include "my_plugin.h"
 
 #include <iostream>
@@ -26,8 +28,9 @@ my_PLUGIN::my_PLUGIN(gcc::context *ctxt, struct plugin_argument *arguments, int 
 }
 
 extern void dump_function_header(FILE *, tree, dump_flags_t);
+extern void make_decl_rtl (tree);
 
-// ripped from 
+// ripped from print-rtl-function.cc
 static void
 print_edge (FILE *outfile, edge e, bool from)
 {
@@ -115,13 +118,50 @@ can_have_basic_block_p (const rtx_insn *insn)
   return true;
 }
 
+static void
+print_any_param_name (FILE *outfile, tree arg)
+{
+  if (DECL_NAME (arg))
+    fprintf (outfile, " \"%s\"", IDENTIFIER_POINTER (DECL_NAME (arg)));
+}
+
+/* Print a "(param)" directive for ARG to OUTFILE.  */
+
+static void
+print_param (FILE *outfile, rtx_writer &w, tree arg)
+{
+  fprintf (outfile, "  (param");
+  print_any_param_name (outfile, arg);
+  fprintf (outfile, "\n");
+
+  /* Print the value of DECL_RTL (without lazy-evaluation).  */
+  fprintf (outfile, "    (DECL_RTL ");
+  w.print_rtx (DECL_RTL_IF_SET (arg));
+  w.finish_directive ();
+
+  /* Print DECL_INCOMING_RTL.  */
+  fprintf (outfile, "    (DECL_RTL_INCOMING ");
+  w.print_rtx (DECL_INCOMING_RTL (arg));
+  fprintf (outfile, ")");
+
+  w.finish_directive ();
+}
 
 unsigned int my_PLUGIN::execute(function *fun)
 {
+  rtx_reuse_manager r;
+  rtx_writer w (stdout, 0, false, false, &r);  
   // 1) Find the name of the function
   char* funName = (char*)IDENTIFIER_POINTER (DECL_NAME (current_function_decl) );
-  std::cerr << "execute on " << funName << " file " << main_input_filename << "\n";
+  tree fdecl = fun->decl;
+  const char *dname = lang_hooks.decl_printable_name (fdecl, 1);
+  std::cerr << "execute on " << funName << " (" << dname << ") file " << main_input_filename << "\n";
   dump_function_header(stdout, fun->decl, (dump_flags_t)0);
+  // dump params
+  /* Params.  */
+  for (tree arg = DECL_ARGUMENTS (fdecl); arg; arg = DECL_CHAIN (arg))
+    print_param (stdout, w, arg);
+
   basic_block bb;
   FOR_ALL_BB_FN(bb, fun)
   { // Loop over all Basic Blocks in the function, cfun = current function
@@ -131,10 +171,11 @@ unsigned int my_PLUGIN::execute(function *fun)
       FOR_BB_INSNS(bb, insn)
       {
         // Loop over all rtx statements in the Basick Block
-        if( NONDEBUG_INSN_P(insn) ){            // Filter all actual code statements
+        // if( NONDEBUG_INSN_P(insn) ){            // Filter all actual code statements
             //print_simple_rtl(fp, insn);
-            print_rtl_single(stdout, insn);             // print to file
-        }
+            // print_rtl_single(stdout, insn);             // print to file
+            w.print_rtl_single_with_indent(insn, 0);
+        // }
       }
       end_any_block (stdout, bb);
       fprintf(stdout,"\n----------------------------------------------------------------\n\n");
