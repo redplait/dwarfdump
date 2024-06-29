d72fe0beb59da5dc3b16847ce8cd79bcdb4a5370
redp <redplait@gmail.com>
2024-06-29 14:34:57 +0300

    test case for class static var

--- gptest/test/vtest.cc
+++ gptest/test/vtest.cc
@@ -15,8 +15,13 @@ class abstract
     : m_worker(do_work),
       bit_1(0),
       bit_2(1)
-   {}
-   virtual ~abstract() = default;
+   {
+     s_abs_cnt++;
+   }
+   virtual ~abstract()
+   {
+     s_abs_cnt--;
+   }
    virtual void dump(FILE *fp)
    {
      fprintf(fp, "anstract");
@@ -43,16 +48,36 @@ class abstract
    {
      return bit_1 ? &abstract::set_field : &abstract::set_field2;
    }
+   virtual void dump_2(FILE *fp)
+   {
+     fprintf(fp, "name %s bit_1 %d bit_2 %d\n", get_name(), bit_1, bit_2);
+   }
    // fields
    int bit_1: 1;
    int bit_2: 2;
    work_pfn m_worker;
+   static int s_abs_cnt;
 };
 
-class derived: public abstract
+int abstract::s_abs_cnt = 0;
+
+class not_so_abstract: public virtual abstract
+{
+ public:
+   not_so_abstract(): abstract()
+   {
+     bit_2 = 3;
+   }
+   virtual const char *get_name()
+   {
+     return "not_so_abstract";
+   }
+};
+
+class derived: public not_so_abstract, public virtual abstract
 {
   public:
-   derived(): abstract()
+   derived(): not_so_abstract()
    {
      m_worker = do_work2;
      bit_1 = 1;
@@ -60,7 +85,7 @@ class derived: public abstract
    virtual ~derived() = default;
    virtual void dump(FILE *fp)
    {
-     fprintf(fp, "derived");
+     fprintf(fp, "derived, cnt %d\n", s_abs_cnt);
    }
    virtual const char *get_name()
    {
@@ -88,12 +113,15 @@ const char *do_work2(abstract *a)
 int main(int argc, char **argv)
 {
   abstract *a;
-  if ( argc > 1 )
-    a = new derived();
+  if ( argc > 2 )
+   a = new derived();
+  else if ( argc > 1 )
+   a = new not_so_abstract();
   else
     a = new abstract();
   a->m_worker(a);
   auto iref = a->get_ref2();
   printf("result %s\n", (a->*iref)(argc));
+  a->dump_2(stdout);
   delete a;
 }
