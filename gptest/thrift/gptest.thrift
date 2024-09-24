7d26bd51b25f993ee5a43610ec1e7266bcbf642b
redp <redplait@gmail.com>
2024-09-24 20:32:05 +0300

    rpc interface based on apache thrift, to compile use thrift --gen cpp:moveable_types gptest.thrift

---
+++ gptest/thrift/gptest.thrift
@@ -0,0 +1,34 @@
+struct FCheck {
+ 1: i32 id,
+ 2: i32 skip
+}
+
+/* one error record */
+struct FErrRec {
+ 1: i32 bid,
+ 2: string msg
+}
+
+/* one xref item */
+struct FRefs {
+ 1: i32 bid,
+ 2: i32 arg,
+ 3: i32 what,
+ 4: i8 kind
+}
+
+/* body of function, name added in check_function */
+struct FFunc {
+ 1: i32 id,
+ 2: i32 bbcount,
+ 3: string fname, /* file name */
+ 4: list<FErrRec> errors,
+ 5: list<FRefs> refs
+}
+
+service Symref {
+  FCheck check_function(1: string funcname),
+  i32 check_sym(1: string name),
+  void add_func(1: FFunc f),
+  void quit()
+}
