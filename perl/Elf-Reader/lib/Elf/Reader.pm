5c70916f7ee9ba5e915d28e08cf8cf26da0284ef
redp <redplait@gmail.com>
2024-10-16 16:05:04 +0300

    add more elf constants

--- perl/Elf-Reader/lib/Elf/Reader.pm
+++ perl/Elf-Reader/lib/Elf/Reader.pm
@@ -49,6 +49,146 @@ use constant {
  ELFDATA2MSB => 2,
 };
 
+# Segment types
+use constant {
+ PT_NULL         => 0,
+ PT_LOAD         => 1,
+ PT_DYNAMIC      => 2,
+ PT_INTERP       => 3,
+ PT_NOTE         => 4,
+ PT_SHLIB        => 5,
+ PT_PHDR         => 6,
+ PT_TLS          => 7,
+ PT_LOOS         => 0X60000000,
+ PT_GNU_EH_FRAME => 0X6474E550,
+ PT_GNU_STACK    => 0X6474E551,
+ PT_GNU_RELRO    => 0X6474E552,
+ PT_GNU_PROPERTY => 0X6474E553,
+ PT_GNU_MBIND_LO => 0X6474E555,
+ PT_GNU_MBIND_HI => 0X6474F554,
+ PT_PAX_FLAGS    => 0X65041580,
+ PT_OPENBSD_RANDOMIZE => 0X65A3DBE6,
+ PT_OPENBSD_WXNEEDED  => 0X65A3DBE7,
+ PT_OPENBSD_BOOTDATA  => 0X65A41BE6,
+ PT_SUNWSTACK         => 0X6FFFFFFB,
+ PT_HIOS              => 0X6FFFFFFF,
+ PT_LOPROC            => 0X70000000,
+ PT_HIPROC            => 0X7FFFFFFF,
+};
+
+# segment flags
+use constant {
+ PF_X        => 1,
+ PF_W        => 2,
+ PF_R        => 4,
+ PF_MASKOS   => 0x0ff00000,
+ PF_MASKPROC => 0xf0000000,
+};
+
+# Section indexes
+use constant {
+ SHN_UNDEF     => 0,
+ SHN_LORESERVE => 0xFF00,
+ SHN_LOPROC    => 0xFF00,
+ SHN_HIPROC    => 0xFF1F,
+ SHN_LOOS      => 0xFF20,
+ SHN_HIOS      => 0xFF3F,
+ SHN_ABS       => 0xFFF1,
+ SHN_COMMON    => 0xFFF2,
+ SHN_XINDEX    => 0xFFFF,
+ SHN_HIRESERVE => 0xFFFF,
+};
+
+# Section types
+use constant {
+ SHT_NULL => 0,
+ SHT_PROGBITS => 1,
+ SHT_SYMTAB => 2,
+ SHT_STRTAB => 3,
+ SHT_RELA => 4,
+ SHT_HASH => 5,
+ SHT_DYNAMIC => 6,
+ SHT_NOTE => 7,
+ SHT_NOBITS => 8,
+ SHT_REL => 9,
+ SHT_SHLIB => 10,
+ SHT_DYNSYM => 11,
+ SHT_INIT_ARRAY => 14,
+ SHT_FINI_ARRAY => 15,
+ SHT_PREINIT_ARRAY => 16,
+ SHT_GROUP => 17,
+ SHT_SYMTAB_SHNDX => 18,
+ SHT_GNU_ATTRIBUTES => 0x6ffffff5,
+ SHT_GNU_HASH => 0x6ffffff6,
+ SHT_GNU_LIBLIST => 0x6ffffff7,
+ SHT_CHECKSUM => 0x6ffffff8,
+ SHT_LOSUNW => 0x6ffffffa,
+ SHT_SUNW_move => 0x6ffffffa,
+ SHT_SUNW_COMDAT => 0x6ffffffb,
+ SHT_SUNW_syminfo => 0x6ffffffc,
+ SHT_GNU_verdef => 0x6ffffffd,
+ SHT_GNU_verneed => 0x6ffffffe,
+ SHT_GNU_versym => 0x6fffffff,
+ SHT_LOOS => 0x60000000,
+ SHT_HIOS => 0x6fffffff,
+ SHT_LOPROC => 0x70000000,
+ SHT_HIPROC => 0x7FFFFFFF,
+ SHT_LOUSER => 0x80000000,
+ SHT_HIUSER => 0xFFFFFFFF,
+};
+
+# section attribs
+use constant {
+ SHF_WRITE => 0x1,
+ SHF_ALLOC => 0x2,
+ SHF_EXECINSTR => 0x4,
+ SHF_MERGE => 0x10,
+ SHF_STRINGS => 0x20,
+ SHF_INFO_LINK => 0x40,
+ SHF_LINK_ORDER => 0x80,
+ SHF_OS_NONCONFORMING => 0x100,
+ SHF_GROUP => 0x200,
+ SHF_TLS => 0x400,
+ SHF_MASKOS => 0x0ff00000,
+ SHF_MASKPROC => 0xF0000000,
+};
+
+# symbol binding
+use constant {
+ STB_LOCAL => 0,
+ STB_GLOBAL => 1,
+ STB_WEAK => 2,
+ STB_LOOS => 10,
+ STB_HIOS => 12,
+ STB_MULTIDEF => 13,
+ STB_LOPROC => 13,
+ STB_HIPROC => 15,
+};
+
+# symbol types
+use constant {
+ STT_NOTYPE => 0,
+ STT_OBJECT => 1,
+ STT_FUNC => 2,
+ STT_SECTION => 3,
+ STT_FILE => 4,
+ STT_COMMON => 5,
+ STT_TLS => 6,
+ STT_LOOS => 10,
+ STT_AMDGPU_HSA_KERNEL => 10,
+ STT_HIOS => 12,
+ STT_LOPROC => 13,
+ STT_HIPROC => 15,
+};
+
+# symbol visibility
+use constant {
+ STV_DEFAULT => 0,
+ STV_INTERNAL => 1,
+ STV_HIDDEN => 2,
+ STV_PROTECTED => 3,
+};
+
 our @EXPORT = qw(
  ET_NONE
  ET_REL
@@ -65,6 +205,114 @@ our @EXPORT = qw(
  ELFDATANONE
  ELFDATA2LSB
  ELFDATA2MSB
+ PT_NULL
+ PT_LOAD
+ PT_DYNAMIC
+ PT_INTERP
+ PT_NOTE
+ PT_SHLIB
+ PT_PHDR
+ PT_TLS
+ PT_LOOS
+ PT_GNU_EH_FRAME
+ PT_GNU_STACK
+ PT_GNU_RELRO
+ PT_GNU_PROPERTY
+ PT_GNU_MBIND_LO
+ PT_GNU_MBIND_HI
+ PT_PAX_FLAGS
+ PT_OPENBSD_RANDOMIZE
+ PT_OPENBSD_WXNEEDED
+ PT_OPENBSD_BOOTDATA
+ PT_SUNWSTACK
+ PT_HIOS
+ PT_LOPROC
+ PT_HIPROC
+ PF_X
+ PF_W
+ PF_R
+ PF_MASKOS
+ PF_MASKPROC
+ SHN_UNDEF
+ SHN_LORESERVE
+ SHN_LOPROC
+ SHN_HIPROC
+ SHN_LOOS
+ SHN_HIOS
+ SHN_ABS
+ SHN_COMMON
+ SHN_XINDEX
+ SHN_HIRESERVE
+SHT_NULL
+SHT_PROGBITS
+SHT_SYMTAB
+SHT_STRTAB
+SHT_RELA
+SHT_HASH
+SHT_DYNAMIC
+SHT_NOTE
+SHT_NOBITS
+SHT_REL
+SHT_SHLIB
+SHT_DYNSYM
+SHT_INIT_ARRAY
+SHT_FINI_ARRAY
+SHT_PREINIT_ARRAY
+SHT_GROUP
+SHT_SYMTAB_SHNDX
+SHT_GNU_ATTRIBUTES
+SHT_GNU_HASH
+SHT_GNU_LIBLIST
+SHT_CHECKSUM
+SHT_LOSUNW
+SHT_SUNW_move
+SHT_SUNW_COMDAT
+SHT_SUNW_syminfo
+SHT_GNU_verdef
+SHT_GNU_verneed
+SHT_GNU_versym
+SHT_LOOS
+SHT_HIOS
+SHT_LOPROC
+SHT_HIPROC
+SHT_LOUSER
+SHT_HIUSER
+SHF_WRITE
+SHF_ALLOC
+SHF_EXECINSTR
+SHF_MERGE
+SHF_STRINGS
+SHF_INFO_LINK
+SHF_LINK_ORDER
+SHF_OS_NONCONFORMING
+SHF_GROUP
+SHF_TLS
+SHF_MASKOS
+SHF_MASKPROC
+STB_LOCAL
+STB_GLOBAL
+STB_WEAK
+STB_LOOS
+STB_HIOS
+STB_MULTIDEF
+STB_LOPROC
+STB_HIPROC
+STT_NOTYPE
+STT_OBJECT
+STT_FUNC
+STT_SECTION
+STT_FILE
+STT_COMMON
+STT_TLS
+STT_LOOS
+STT_AMDGPU_HSA_KERNEL
+STT_HIOS
+STT_LOPROC
+STT_HIPROC
+STV_DEFAULT
+STV_INTERNAL
+STV_HIDDEN
+STV_PROTECTED
 );
 
 our $VERSION = '0.01';
@@ -87,21 +335,35 @@ Elf::Reader - Perl extension for blah blah blah
 =head1 SYNOPSIS
 
   use Elf::Reader;
-  blah blah blah
+  my $er = Elf::Reader->new($filename);
+  # for sections
+  my $secs = $er->secs();
+  print "sections ", scalar( @$secs );
+  # for segments
+  my $segs = $er->segs();
+  print "segments ", scalar( @$segs );
+
+for reloc names use Elf::Relocs;
 
 =head1 DESCRIPTION
 
-Stub documentation for Elf::Reader, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+Perl binding for ELFIO library (https://github.com/serge1/ELFIO/commits/main/)
+Support array-like iterators for sections and segments
 
-Blah blah blah.
+segs items are arrays with indexes
+ 0 - index
+ 1 - type
+ 2 - flags
+ 3 - align
+ 4 - virtual address, 64bit
+ 5 - physical address, 64bit
+ 6 - file size
+ 7 - memory size
+ 8 - offset, 64bit
 
 =head2 EXPORT
 
-None by default.
-
-
+Names of ELF constants
 
 =head1 SEE ALSO
 
@@ -116,7 +378,7 @@ If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
-redp, E<lt>redp@E<gt>
+redp, <lt>redp@mail.ru<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
