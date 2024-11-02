f49e9003895e32a86c32ca1b7f099a69f9feaee6
redp <redplait@gmail.com>
2024-11-02 18:08:50 +0300

    some doc

--- perl/Dwarf-Loader/lib/Dwarf/Loader.pm
+++ perl/Dwarf-Loader/lib/Dwarf/Loader.pm
@@ -130,11 +130,30 @@ C++ sources for dwarf parser itself located in ../.. dir
 
 =head1 DESCRIPTION
 
-Stub documentation for Dwarf::Loader, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+In DWARF all revolved around so called DIE - it representing some tag (tag_id is essentially offset in section .debug_abbrev)
+ and all attributes, in my dwarf library this is TreeBuilder::Element (exposed to perl as Dwarf::Loader::Element)
+There is problem when you read some tag - is it full-fledged DIE or just some limited nameless anchor for another type like
+ DW_TAG_const_type
+  DW_AT_type xxx
+xxx DW_TAG_pointer_type
+  DW_AT_type yyy
+yyy DW_TAG_some_real_type
+ 
 
-Blah blah blah.
+For some tags you can safely predict to which class it falls - like DW_TAG_enumerator or DW_TAG_member but in general you can't
+So there are two approach how to read and parse DWARF debug info
+1) do all in one pass. Obvious drawback is memory comsumption - each DIE should keep all possible attributes (see Table A.1
+ on page 266 for list of all possible attributes for each tag)
+2) scan first pass only to build indexes of named/addressable DIEs and then read and parse each DIE on demand. In such case
+we need to do lots of book-keeping for each CU - like store bases for things like loclists/offsets/addresses/rnglists etc
+
+I decided to use first approach - despite the fact that on x64 sizeof(Element) is 224 bytes as of this writing. To save space
+some tags keeping in smaller types:
+ - DW_TAG_inheritance is type Parent (exposed to perl as Dwarf::Loader::Parent)
+ - DW_TAG_formal_parameter is type FormalParam (exposed to perl as Dwarf::Loader::Param)
+ - DW_TAG_enumerator is type EnumItem (can be accessed via Dwarf::Loader::EnumIterator)
+and keep lists of nested elements in structure Compound (sizeof is 200 bytes). Can't say I'm satisfied with the result -
+in typical DWARF lots of xxx_type tags occupy only several bytes vs 224 in my library
 
 =head2 EXPORT
 
@@ -144,10 +163,7 @@ None by default.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+ DWARF Version 5 Standard: https://dwarfstd.org/doc/DWARF5.pdf
 
 If you have a mailing list set up for your module, mention it here.
 
